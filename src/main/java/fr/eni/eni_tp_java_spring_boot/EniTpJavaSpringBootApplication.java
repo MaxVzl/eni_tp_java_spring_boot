125ea9c5f3de00118b79a27269794cea2c972a17
MaxVzl <maxime.vozelle@gmail.com>
2025-07-08 15:45:31 +0200

    init

---
+++ src/main/java/fr/eni/eni_tp_java_spring_boot/EniTpJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.eni_tp_java_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniTpJavaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EniTpJavaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/eni_tp_java_spring_boot/EniTpJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.eni_tp_java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniTpJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
