eabbc1b59ea02a416d578247fe23a27be9710e2e
MaxVzl <maxime.vozelle@gmail.com>
2025-07-09 12:19:11 +0200

    create client repo test

--- src/test/java/fr/eni/eni_tp_java_spring_boot/repositories/ClientRepositoryTest.java
+++ src/test/java/fr/eni/eni_tp_java_spring_boot/repositories/ClientRepositoryTest.java
@@ -1,5 +1,19 @@
 package fr.eni.eni_tp_java_spring_boot.repositories;
 
-public class ClientRepositoryTest {
+import fr.eni.eni_tp_java_spring_boot.services.ClientService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+public class ClientRepositoryTest {
+    private ClientService clientService;
+
+    @Mock
+    private ClientRepository clientRepository;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+        clientService = new ClientService(clientRepository);
+    }
 }
